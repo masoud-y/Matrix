fbd95b937734ba6c77e6c4e61f28eef2a036a0c1
Dr. Yari <masoud.yari@tamucc.edu>
2017-04-17 10:48:02 -0500

    Initial Commit

---
+++ Matrix/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Matrix
+//
+//  Created by Masoud on 4/17/17.
+//  Copyright © 2017 Msd. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
